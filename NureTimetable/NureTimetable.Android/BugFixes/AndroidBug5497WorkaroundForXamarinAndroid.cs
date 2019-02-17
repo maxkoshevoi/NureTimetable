2091ff46aec19a950571e6ec1ea1c3465f595599
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2019-02-17 21:35:39 +0200

    - Added fix for the keyboard so it doesn't overlap the grid icons above keyboard etc, and makes Android 5+ work as AdjustResize in Android 4 - Added ability to perform in-app purchase

---
+++ NureTimetable/NureTimetable.Android/BugFixes/AndroidBug5497WorkaroundForXamarinAndroid.cs
@@ -0,0 +1,80 @@
+﻿
+using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace NureTimetable.Droid.BugFixes
+{
+    public class AndroidBug5497WorkaroundForXamarinAndroid
+    {
+        private readonly View mChildOfContent;
+        private int usableHeightPrevious;
+        private FrameLayout.LayoutParams frameLayoutParams;
+
+        public static void assistActivity(Activity activity, IWindowManager windowManager)
+        {
+            new AndroidBug5497WorkaroundForXamarinAndroid(activity, windowManager);
+        }
+
+        private AndroidBug5497WorkaroundForXamarinAndroid(Activity activity, IWindowManager windowManager)
+        {
+
+            var softButtonsHeight = getSoftbuttonsbarHeight(windowManager);
+
+            var content = (FrameLayout)activity.FindViewById(Android.Resource.Id.Content);
+            mChildOfContent = content.GetChildAt(0);
+            var vto = mChildOfContent.ViewTreeObserver;
+            vto.GlobalLayout += (sender, e) => possiblyResizeChildOfContent(softButtonsHeight);
+            frameLayoutParams = (FrameLayout.LayoutParams)mChildOfContent.LayoutParameters;
+        }
+
+        private void possiblyResizeChildOfContent(int softButtonsHeight)
+        {
+            var usableHeightNow = computeUsableHeight();
+            if (usableHeightNow != usableHeightPrevious)
+            {
+                var usableHeightSansKeyboard = mChildOfContent.RootView.Height - softButtonsHeight;
+                var heightDifference = usableHeightSansKeyboard - usableHeightNow;
+                if (heightDifference > (usableHeightSansKeyboard / 4))
+                {
+                    // keyboard probably just became visible
+                    frameLayoutParams.Height = usableHeightSansKeyboard - heightDifference + (softButtonsHeight / 2);
+                }
+                else
+                {
+                    // keyboard probably just became hidden
+                    frameLayoutParams.Height = usableHeightSansKeyboard;
+                }
+                mChildOfContent.RequestLayout();
+                usableHeightPrevious = usableHeightNow;
+            }
+        }
+
+        private int computeUsableHeight()
+        {
+            var r = new Rect();
+            mChildOfContent.GetWindowVisibleDisplayFrame(r);
+            return (r.Bottom - r.Top);
+        }
+
+        private int getSoftbuttonsbarHeight(IWindowManager windowManager)
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var metrics = new DisplayMetrics();
+                windowManager.DefaultDisplay.GetMetrics(metrics);
+                int usableHeight = metrics.HeightPixels;
+                windowManager.DefaultDisplay.GetRealMetrics(metrics);
+                int realHeight = metrics.HeightPixels;
+                if (realHeight > usableHeight)
+                    return realHeight - usableHeight;
+                else
+                    return 0;
+            }
+            return 0;
+        }
+    }
+}
--- NureTimetable/NureTimetable.Android/MainActivity.cs
+++ NureTimetable/NureTimetable.Android/MainActivity.cs
@@ -6,11 +6,17 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.InAppBilling;
+using Android.Content;
+using Plugin.CurrentActivity;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using NureTimetable.Droid.BugFixes;
 
 namespace NureTimetable.Droid
 {
     [Activity(Label = "NureTimetable", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -18,8 +24,23 @@ namespace NureTimetable.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Forms.Init(this, savedInstanceState);
+            CrossCurrentActivity.Current.Activity = this;
             LoadApplication(new App());
+            
+            // Fix the keyboard so it doesn't overlap the grid icons above keyboard etc, and makes Android 5+ work as AdjustResize in Android 4
+            Window.SetSoftInputMode(SoftInput.AdjustResize);
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                // Bug in Android 5+, this is an adequate workaround
+                AndroidBug5497WorkaroundForXamarinAndroid.assistActivity(this, WindowManager);
+            }
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            InAppBillingImplementation.HandleActivityResult(requestCode, resultCode, data);
         }
     }
 }
