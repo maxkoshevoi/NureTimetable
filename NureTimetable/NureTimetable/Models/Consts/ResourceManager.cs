b8a6ffcaa4e4cf599d10e9b4f0fbd4760af9236b
Lyubchenko Yuriy <undeadspez@gmail.com>
2019-02-20 00:13:15 +0200

    Update resource manager

--- NureTimetable/NureTimetable/Models/Consts/ResourceManager.cs
+++ NureTimetable/NureTimetable/Models/Consts/ResourceManager.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Linq;
-using System.Text;
+﻿using Xamarin.Forms;
 
 namespace NureTimetable.Models.Consts
 {
     public static class ResourceManager
     {
-        public static string KeyForEventColor(Event e)
-        {
-            string compType = e.Type.ToLower();
-
-            return KnownEventTypes.Values.Contains(compType)
-                ? $"{compType}Color"
-                : "defaultColor";
-        }
+        public static Color EventColor(Event e)
+            => (Color) App.Current.Resources[ResourceNames.EventColor(e.Type)];
     }
 }
---
+++ NureTimetable/NureTimetable/Models/Consts/ResourceNames.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace NureTimetable.Models.Consts
+{
+    class ResourceNames
+    {
+        public static string EventColor(string type)
+        {
+            var comparableType = type.ToLower();
+
+            return KnownEventTypes.Values.Contains(comparableType)
+                ? $"{comparableType}Color"
+                : "defaultColor";
+        }
+    }
+}
