df0ae74ffb48797d63a722d9619c686845984671
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2019-02-21 21:32:07 +0200

    Disabled changes in Keys.cs to be committed

--- NureTimetable/NureTimetable/Models/Consts/Keys.cs
+++ NureTimetable/NureTimetable/Models/Consts/Keys.cs
@@ -1,5 +1,7 @@
 ﻿namespace NureTimetable.Models.Consts
 {
+    //Don't forget to use following git command to prevent changes in this file to be committed
+    //git update-index --assume-unchanged NureTimetable\NureTimetable\Models\Consts\Keys.cs
     public static class Keys
     {
         public const string SyncfusionLicenseKey = "<insert key here>";
