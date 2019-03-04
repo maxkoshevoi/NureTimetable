27b1d7fad51909200ead39447b85fe3ee1b7f46a
Lyubchenko Yuriy <undeadspez@gmail.com>
2019-03-04 20:58:22 +0200

    Restore file

---
+++ NureTimetable/NureTimetable/ViewModels/System/MenuViewmodel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using NureTimetable.Core.Localization;
+using NureTimetable.Models.System;
+
+namespace NureTimetable.ViewModels.System
+{
+    public class MenuViewModel
+    {
+        private IList<HomeMenuItem> _menuItems;
+
+        public IList<HomeMenuItem> MenuItems => _menuItems ?? (_menuItems = GetMenuItems());
+
+        private IList<HomeMenuItem> GetMenuItems()
+        {
+            var menuItemModels = new List<HomeMenuItem>
+            {
+                    new HomeMenuItem
+                    {
+                        Id = MenuItemType.Timetable,
+                        Title = "Расписание"
+                    },
+                    new HomeMenuItem
+                    {
+                        Id = MenuItemType.About,
+                        Title = "О программе"
+                    },
+                    new HomeMenuItem
+                    {
+                        Id = MenuItemType.Donate,
+                        Title = "Пожертвовать"
+}
+            };
+
+            return menuItemModels;
+        }
+    }
+}
