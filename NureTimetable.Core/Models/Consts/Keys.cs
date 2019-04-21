daa3f32c29a11a8382499f45bb0ec02204410cc0
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2019-04-21 03:03:40 +0300

    Fixed instructions for Keys file

--- NureTimetable.Core/Models/Consts/Keys.cs
+++ NureTimetable.Core/Models/Consts/Keys.cs
@@ -1,7 +1,9 @@
 ﻿namespace NureTimetable.Core.Models.Consts
 {
-    //Don't forget to use following git command to prevent changes in this file to be committed
-    //git update-index --assume-unchanged "NureTimetable\NureTimetable.Core\Models\Consts\Keys.cs"
+    // Don't forget to use following git command to prevent changes in this file to be committed:
+    // git update-index --assume-unchanged "NureTimetable.Core\Models\Consts\Keys.cs"
+    // To make changes in this file visible again, use following command:
+    // git update-index --no-assume-unchanged "NureTimetable.Core\Models\Consts\Keys.cs"
     public static class Keys
     {
         public const string SyncfusionLicenseKey = null;
