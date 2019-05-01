97f41d2c7d6a684d3bac97e3020f9d0e4cc8554f
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2019-05-01 22:29:38 +0300

    Added check for single AutoMapper initialization

--- NureTimetable.DAL/DALBase.cs
+++ NureTimetable.DAL/DALBase.cs
@@ -10,36 +10,48 @@ namespace NureTimetable.DAL
         public static void Init()
         {
             // Yeah static constructor is bad, but I need to initialize AutoMapper somewhere
+            
+            bool isMapperInitialized = true;
+            try
+            {
+                var mapper = Mapper.Instance;
+            }
+            catch (Exception)
+            {
+                isMapperInitialized = false;
+            }
+            if (!isMapperInitialized)
+            {
+                var localTimezone = TimeZoneInfo.Local;
+                Mapper.Initialize(cfg => {
+                    // UniversityEntitiesRepository
+                    cfg.CreateMap<Cist.Group, Local.Group>();
+                    cfg.CreateMap<Cist.Faculty, Local.BaseEntity<long>>()
+                        .ForMember("FullName", opt => opt.MapFrom(src => src.ShortName))
+                        .ForMember("ShortName", opt => opt.MapFrom(src => src.FullName));
+                    cfg.CreateMap<Cist.Direction, Local.BaseEntity<long>>()
+                        .ForMember("FullName", opt => opt.MapFrom(src => src.ShortName))
+                        .ForMember("ShortName", opt => opt.MapFrom(src => src.FullName));
+                    cfg.CreateMap<Cist.Speciality, Local.BaseEntity<long>>();
 
-            var localTimezone = TimeZoneInfo.Local;
-            Mapper.Initialize(cfg => {
-                // UniversityEntitiesRepository
-                cfg.CreateMap<Cist.Group, Local.Group>();
-                cfg.CreateMap<Cist.Faculty, Local.BaseEntity<long>>()
-                    .ForMember("FullName", opt => opt.MapFrom(src => src.ShortName))
-                    .ForMember("ShortName", opt => opt.MapFrom(src => src.FullName));
-                cfg.CreateMap<Cist.Direction, Local.BaseEntity<long>>()
-                    .ForMember("FullName", opt => opt.MapFrom(src => src.ShortName))
-                    .ForMember("ShortName", opt => opt.MapFrom(src => src.FullName));
-                cfg.CreateMap<Cist.Speciality, Local.BaseEntity<long>>();
+                    cfg.CreateMap<Cist.Teacher, Local.Teacher>()
+                        .ForMember("Name", opt => opt.MapFrom(src => src.FullName));
+                    cfg.CreateMap<Cist.Department, Local.BaseEntity<long>>();
 
-                cfg.CreateMap<Cist.Teacher, Local.Teacher>()
-                    .ForMember("Name", opt => opt.MapFrom(src => src.FullName));
-                cfg.CreateMap<Cist.Department, Local.BaseEntity<long>>();
+                    cfg.CreateMap<Cist.Room, Local.Room>()
+                        .ForMember("Name", opt => opt.MapFrom(src => src.ShortName));
+                    cfg.CreateMap<Cist.RoomType, Local.RoomType>();
+                    cfg.CreateMap<Cist.Building, Local.BaseEntity<string>>();
 
-                cfg.CreateMap<Cist.Room, Local.Room>()
-                    .ForMember("Name", opt => opt.MapFrom(src => src.ShortName));
-                cfg.CreateMap<Cist.RoomType, Local.RoomType>();
-                cfg.CreateMap<Cist.Building, Local.BaseEntity<string>>();
-
-                // EventsRepository
-                cfg.CreateMap<Cist.Event, Local.Event>()
-                    .ForMember("RoomName", opt => opt.MapFrom(src => src.Room))
-                    .ForMember("Start", opt => opt.MapFrom(src => src.StartTime.Add(localTimezone.GetUtcOffset(src.StartTime))))
-                    .ForMember("End", opt => opt.MapFrom(src => src.EndTime.Add(localTimezone.GetUtcOffset(src.EndTime))));
-                cfg.CreateMap<Cist.EventType, Local.EventType>();
-                cfg.CreateMap<Cist.Lesson, Local.Lesson>();
-            });
+                    // EventsRepository
+                    cfg.CreateMap<Cist.Event, Local.Event>()
+                        .ForMember("RoomName", opt => opt.MapFrom(src => src.Room))
+                        .ForMember("Start", opt => opt.MapFrom(src => src.StartTime.Add(localTimezone.GetUtcOffset(src.StartTime))))
+                        .ForMember("End", opt => opt.MapFrom(src => src.EndTime.Add(localTimezone.GetUtcOffset(src.EndTime))));
+                    cfg.CreateMap<Cist.EventType, Local.EventType>();
+                    cfg.CreateMap<Cist.Lesson, Local.Lesson>();
+                });
+            }
         }
     }
 }
