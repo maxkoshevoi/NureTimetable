027e28560dd8f6a3af5a9ed6767716592b05f220
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2020-11-16 00:17:51 +0200

    Bring back IActivityManager

---
+++ NureTimetable/NureTimetable.Android/Dependences/ActivityManager.cs
@@ -0,0 +1,16 @@
+﻿using NureTimetable.Core.Models.InterplatformCommunication;
+using NureTimetable.Droid.Dependences;
+using Plugin.CurrentActivity;
+
+[assembly: Xamarin.Forms.Dependency(typeof(ActivityManager))]
+namespace NureTimetable.Droid.Dependences
+{
+    public class ActivityManager : IActivityManager
+    {
+        public void Recreate()
+        {
+            var activity = CrossCurrentActivity.Current.Activity;
+            activity.Recreate();
+        }
+    }
+}
