120c5f2fbf9ef357616ae461bd1fd717aa0b21a5
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-04-15 18:42:36 +0300

    Change app language on device language change

---
+++ NureTimetable/NureTimetable.Android/Receivers/LocaleChangeReceiver.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Java.Util;
+using NureTimetable.Core.Models.Settings;
+using NureTimetable.DAL;
+using System.Globalization;
+using Xamarin.CommunityToolkit.Helpers;
+
+namespace NureTimetable.Droid.Receivers
+{
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new[] { Intent.ActionLocaleChanged })]
+    public class LocaleChangeReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(Locale.Default.Language);
+            if (SettingsRepository.Settings.Language == AppLanguage.FollowSystem)
+            {
+                LocalizationResourceManager.Current.CurrentCulture = CultureInfo.CurrentCulture;
+            }
+        }
+    }
+}
