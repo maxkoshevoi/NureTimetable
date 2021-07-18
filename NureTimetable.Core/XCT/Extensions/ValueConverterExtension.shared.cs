eb1e307444320b1f6c0ce0f4efb05a9c6d430817
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-07-18 14:31:54 +0300

    Copy XCT code dirrectly

---
+++ NureTimetable.Core/XCT/Extensions/ValueConverterExtension.shared.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using System;
+
+namespace Xamarin.CommunityToolkit.Extensions.Internals
+{
+    public abstract class ValueConverterExtension : BindableObject, IMarkupExtension<IValueConverter>
+	{
+		public IValueConverter ProvideValue(IServiceProvider serviceProvider)
+			=> (IValueConverter)this;
+
+		object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+			=> ((IMarkupExtension<IValueConverter>)this).ProvideValue(serviceProvider);
+	}
+}
