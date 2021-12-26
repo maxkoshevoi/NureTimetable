4fc6c6e9283fd5ae349c557e1a0166ee22984952
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-12-26 22:27:45 +0200

    Revert "Adds missing closing brackets" json fixer

--- NureTimetable.DAL/Cist/JsonFixers.cs
+++ NureTimetable.DAL/Cist/JsonFixers.cs
@@ -30,18 +30,10 @@ namespace NureTimetable.DAL.Cist
             ":]",
             ":}"
         };
-        private static readonly (char Open, char Close)[] brackets =
-        {
-            ('[', ']'),
-            ('{', '}'),
-        };
-        private static readonly char[] allBrackets = brackets.SelectMany(b => new[] { b.Open, b.Close }).ToArray();
-        private static readonly Stack<char> openBarckets = new();
 
         /// <summary>
         /// 1. Escapes double quotes in json property values
         /// 2. Replaces ":," with ":null,"
-        /// 3. Adds missing closing brackets
         /// </summary>
         public static string TryFix(string invalidJsonStr)
         {
@@ -102,12 +94,6 @@ namespace NureTimetable.DAL.Cist
                 FixNonStringJson(invalidJson, endIndex, nonStringLength);
             }
 
-            if (openBarckets.Any())
-            {
-                invalidJson.Append(new string(openBarckets.Select(ob => brackets.First(b => b.Open == ob).Close).ToArray()));
-                openBarckets.Clear();
-            }
-
             return invalidJson.ToString();
 
             static void FixNonStringJson(StringBuilder invalidJson, int endIndex, int nonStringLength)
@@ -131,30 +117,6 @@ namespace NureTimetable.DAL.Cist
 
             // Add null values instead of empty ones
             newJson = noValue.Aggregate(newJson, (res, nv) => res.Replace(nv, nv.Insert(1, "null")));
-
-            // Add missing closing brackets
-            int startIndex = newJson.IndexOfAny(allBrackets);
-            while (startIndex != notFound)
-            {
-                char bracket = newJson[startIndex];
-
-                if (brackets.Select(b => b.Open).Contains(bracket))
-                {
-                    openBarckets.Push(bracket);
-                }
-                else
-                {
-                    char lastOpenBracket = openBarckets.Pop();
-
-                    char expectedBracket = brackets.First(b => b.Open == lastOpenBracket).Close;
-                    if (expectedBracket != bracket)
-                    {
-                        newJson = newJson.Insert(startIndex, expectedBracket.ToString());
-                    }
-                }
-
-                startIndex = newJson.IndexOfAny(allBrackets, startIndex + 1);
-            }
 
             return newJson;
         }
