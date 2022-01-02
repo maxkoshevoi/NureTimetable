82d0c3a1fc3d02beeca41d073797a51f1e4d6c36
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2022-01-02 16:57:09 +0200

    Remove Plugin.InAppBilling since it doesn't support MAUI yet

--- NureTimetable/UI/ViewModels/Info/DonateViewModel.cs
+++ NureTimetable/UI/ViewModels/Info/DonateViewModel.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.Maui.Controls;
-using NureTimetable.BL;
-using NureTimetable.Core.Extensions;
-using NureTimetable.Core.Localization;
-using Plugin.InAppBilling;
 using Xamarin.CommunityToolkit.Extensions;
 using Xamarin.CommunityToolkit.ObjectModel;
 
@@ -19,8 +15,6 @@ public class DonateViewModel : BaseViewModel
 
     public static async Task BuyProduct(string productId)
     {
-        InAppBillingPurchase? purchase = await InAppPurchase.Buy(productId, true);
-        string message = purchase == null ? LN.PurchaseFailed : LN.ThanksForYourSupport;
-        Shell.Current.CurrentPage.DisplayToastAsync(message).Forget();
+        await Shell.Current.CurrentPage.DisplayToastAsync("Not supported in MAUI yet.");
     }
 }
